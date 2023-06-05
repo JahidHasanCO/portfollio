3019739b4d254d9f75737a5219d987ad5e9d9660
Md. Zahidul Islam <50579963+JahidHasanCO@users.noreply.github.com>
2023-06-05 15:07:06 +0000

    update

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ window.addEventListener("scroll", () => {
 /* =========== User Data =========== */
 
 const counters = [...document.querySelectorAll(".user-data .number")];
-const speed = 5000; // change animation speed here
+const speed = 500; 
 
 const start = () => {
   counters.forEach((counter) => {
@@ -95,6 +95,8 @@ const scroll = ScrollReveal({
 });
 
 scroll.reveal(`.hero .col:last-child`, { delay: 600 });
+
+scroll.reveal(`.projects .col:last-child`, { delay: 600 });
 
 scroll.reveal(
   `.about-1 .col:last-child h3,.about-1 .col:last-child p,
